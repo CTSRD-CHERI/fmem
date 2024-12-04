67086198c49934014635afa08c2533b436064318
Jonathan Woodruff <jonwoodruff@gmail.com>
2024-12-04 12:24:03 +0000

    Update fmem_load_elf.c
    Check equal alignment, and force align up.  The second part is not correct, but should avoid crashing the kernel with an unaligned fmem access.

--- fmem_load_elf.c
+++ fmem_load_elf.c
@@ -104,6 +104,19 @@ void fmem_memcpy(uint64_t dest,
 {
     //printf("fmem_memcpy called; dest == 0x%x, src == 0x%x, n = 0x%x", dest, (int)src, (int)n);
     void *end = src + n;
+    // Do not attempt this copy if source and destination do not have the same alignment.
+    uintptr_t  s  = (uintptr_t)src;
+    if (s % 4 != dst % 4) {
+        printf("error unaligned copy; cannot copy 0x%p into " PRIx64 "\n", src, dest);
+        return;
+    }
+    // Hopefully this alignment does nothing, but if it does, it prevents the kernel from crashing.
+    // This carelessly discards the first few bytes.
+    if (s % 4 != 0) {
+        s += 4 - s % 4;
+        dest += 4 - s % 4;
+        src = (void *)s;
+    }
     for (; src < end; src += 4, dest += 4) {
         uint64_t offset = dest & (~MEM_MASK_1GB);
         if (offset != last_offset) {
@@ -120,11 +133,11 @@ void fmem_memcpy(uint64_t dest,
         }
         uint32_t write_val = ((uint32_t *)src)[0];
         int error = fmem_write(dest & MEM_MASK_1GB, 4, write_val, h2f_fd);
-	if (error != 0) {
-	    printf("error with h2f bridge (write) 0x%" PRIx64 " == 0x%x\n", dest,
-		    write_val);
-	    break;
-	}
+    	if (error != 0) {
+    	    printf("error with h2f bridge (write) 0x%" PRIx64 " == 0x%x\n", dest,
+    		    write_val);
+    	    break;
+    	}
     }
 }
 
